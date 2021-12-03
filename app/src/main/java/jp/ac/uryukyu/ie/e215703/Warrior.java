b30a63424bb866db83374eee872c3a5b14dc2456
Taisei Maetake <e215703@ie.u-ryukyu.ac.jp>
2021-12-04 01:54:11 +0900

    step2

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e215703/Warrior.java
@@ -0,0 +1,26 @@
+package jp.ac.uryukyu.ie.e215703;
+
+public class Warrior extends LivingThing{
+    public Warrior (String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
+    }
+
+    @Override
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
+        }
+    }
+
+    public void attackWithWeponSkill(LivingThing opponent){
+        if(this.dead){
+            return;
+        }
+        int damage = (int)(attack * 1.5);
+        System.out.printf("%sの攻撃！ウェポンスキルを発動！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+        opponent.wounded(damage);
+
+    }
+}
